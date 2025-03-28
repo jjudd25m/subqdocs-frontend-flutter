f7ca269b8fbca6fdbf1512d990fd41b71ad4a7ab
mihir.rajpopat <mihir.rajpopat@esparkbizmail.com>
2025-03-28 12:57:10 +0530

    change the model of the status

--- lib/app/data/service/socket_service.dart
+++ lib/app/data/service/socket_service.dart
@@ -2,8 +2,9 @@ import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class SocketService {
   // IO.Socket socket = IO.io('https://4ef1-103-215-158-90.ngrok-free.app', IO.OptionBuilder().setTransports(['websocket']).build());
-  IO.Socket socket = IO.io('https://stage-api.subqdocs.ai', IO.OptionBuilder().setTransports(['websocket']).build());
+  // IO.Socket socket = IO.io('https://stage-api.subqdocs.ai', IO.OptionBuilder().setTransports(['websocket']).build());
   // IO.Socket socket = IO.io('https://app-api.subqdocs.ai', IO.OptionBuilder().setTransports(['websocket']).build());
 
-  // IO.Socket socket = IO.io('https://dev-api.subqdocs.ai', IO.OptionBuilder().setTransports(['websocket']).build());
+  IO.Socket socket = IO.io('https://dev-api.subqdocs.ai', IO.OptionBuilder().setTransports(['websocket']).build());
+
 }
--- lib/app/models/ChangeModel.dart
+++ lib/app/models/ChangeModel.dart
@@ -1,13 +1,16 @@
 class ChangeStatusModel {
-  List<int>? responseData;
+  ResponseData? responseData;
   String? message;
   bool? toast;
   String? responseType;
 
-  ChangeStatusModel({this.responseData, this.message, this.toast, this.responseType});
+  ChangeStatusModel(
+      {this.responseData, this.message, this.toast, this.responseType});
 
   ChangeStatusModel.fromJson(Map<String, dynamic> json) {
-    responseData = json['responseData'].cast<int>();
+    responseData = json['responseData'] != null
+        ? new ResponseData.fromJson(json['responseData'])
+        : null;
     message = json['message'];
     toast = json['toast'];
     responseType = json['response_type'];
@@ -15,10 +18,73 @@ class ChangeStatusModel {
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['responseData'] = this.responseData;
+    if (this.responseData != null) {
+      data['responseData'] = this.responseData!.toJson();
+    }
     data['message'] = this.message;
     data['toast'] = this.toast;
     data['response_type'] = this.responseType;
+    return data;
+  }
+}
+
+class ResponseData {
+  int? id;
+  int? patientId;
+  String? visitStatus;
+  String? visitDate;
+  String? visitTime;
+  Null? visitType;
+  Null? visitNotes;
+  String? status;
+  Null? visitDetails;
+  String? createdAt;
+  String? updatedAt;
+  Null? deletedAt;
+
+  ResponseData(
+      {this.id,
+        this.patientId,
+        this.visitStatus,
+        this.visitDate,
+        this.visitTime,
+        this.visitType,
+        this.visitNotes,
+        this.status,
+        this.visitDetails,
+        this.createdAt,
+        this.updatedAt,
+        this.deletedAt});
+
+  ResponseData.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    patientId = json['patient_id'];
+    visitStatus = json['visit_status'];
+    visitDate = json['visit_date'];
+    visitTime = json['visit_time'];
+    visitType = json['visit_type'];
+    visitNotes = json['visit_notes'];
+    status = json['status'];
+    visitDetails = json['visit_details'];
+    createdAt = json['created_at'];
+    updatedAt = json['updated_at'];
+    deletedAt = json['deleted_at'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['patient_id'] = this.patientId;
+    data['visit_status'] = this.visitStatus;
+    data['visit_date'] = this.visitDate;
+    data['visit_time'] = this.visitTime;
+    data['visit_type'] = this.visitType;
+    data['visit_notes'] = this.visitNotes;
+    data['status'] = this.status;
+    data['visit_details'] = this.visitDetails;
+    data['created_at'] = this.createdAt;
+    data['updated_at'] = this.updatedAt;
+    data['deleted_at'] = this.deletedAt;
     return data;
   }
 }
