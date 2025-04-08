6ec8307c3955547ce3c83f7b7e742ce6f4a519a9
Kishan <kishan@esparkbizmail.com>
2025-04-08 16:12:43 +0530

    initial setup

---
+++ ios/subqdocsrecording/subqdocsrecordingBundle.swift
@@ -0,0 +1,18 @@
+//
+//  subqdocsrecordingBundle.swift
+//  subqdocsrecording
+//
+//  Created by HariKrishna Kundariya on 08/04/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct subqdocsrecordingBundle: WidgetBundle {
+    var body: some Widget {
+        subqdocsrecording()
+        subqdocsrecordingControl()
+        subqdocsrecordingLiveActivity()
+    }
+}
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -10,7 +10,6 @@
 #include <file_selector_windows/file_selector_windows.h>
 #include <permission_handler_windows/permission_handler_windows_plugin.h>
 #include <record_windows/record_windows_plugin_c_api.h>
-#include <sentry_flutter/sentry_flutter_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
@@ -22,8 +21,6 @@ void RegisterPlugins(flutter::PluginRegistry* registry) {
       registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
   RecordWindowsPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("RecordWindowsPluginCApi"));
-  SentryFlutterPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("SentryFlutterPlugin"));
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
